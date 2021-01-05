415ed41e6ec4274fc3201af3ecd60f53f36e584a
Rodrigues Matthieu <matthieu.rodrigues@epsi.fr>
2021-01-05 14:37:30 +0100

    First commit

---
+++ src/main/java/EclipseIDE/EclipseIDE/App.java
@@ -0,0 +1,13 @@
+package EclipseIDE.EclipseIDE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
